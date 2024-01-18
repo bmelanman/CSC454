bce82bc359588c3ca802dd1b9d59bbe68c4c4ba0
Bryce Melander <brycemelander@gmail.com>
2024-01-18 18:24:03 +0000

    Added VGA driver init and new tests

--- Binux/src/kernel_main.c
+++ Binux/src/kernel_main.c
@@ -13,44 +13,20 @@
 
 #include "keyboard_driver_polling.h"
 #include "printk.h"
+#include "tests.h"
+#include "vga_driver.h"
 
-/* Public Functions */
 int kernel_main( void )
 {
-    printk( "Hello, World!\n" );
-
-    for ( int i = 0; i < 3; i++ )
+    // Initialize the VGA driver
+    if ( vga_driver_init() == FAILURE )
     {
-        printk( "This is printk() call #%d\n", i );
+        printk( "vga_driver_init() failed!\n" );
+
+        return 1;
     }
 
-    // printk( "                                        \n" );
-    // printk( "Testing printk()...                     \n" );
-    // printk( "----------------------------------------\n" );
-    // printk( "Testing MAX and MIN values...           \n" );
-    // printk( "                                        \n" );
-    // printk( "SHRT_MIN.......%hd                      \n", SHRT_MIN );
-    // printk( "SHRT_MAX.......%hd                      \n", SHRT_MAX );
-    // printk( "USHRT_MAX......%hu (0x%hx)              \n", USHRT_MAX, USHRT_MAX );
-    // printk( "                                        \n" );
-    // printk( "INT_MIN........%d                       \n", INT_MIN );
-    // printk( "INT_MAX........%d                       \n", INT_MAX );
-    // printk( "UINT_MAX.......%u (0x%x)                \n", UINT_MAX, UINT_MAX );
-    // printk( "                                        \n" );
-    // printk( "LONG_MIN.......%ld                      \n", LONG_MIN );
-    // printk( "LONG_MAX.......%ld                      \n", LONG_MAX );
-    // printk( "ULONG_MAX......%lu (0x%lx)              \n", ULONG_MAX, ULONG_MAX );
-    // printk( "                                        \n" );
-    // printk( "LLONG_MIN......%lld                     \n", LLONG_MIN );
-    // printk( "LLONG_MAX......%lld                     \n", LLONG_MAX );
-    // printk( "ULLONG_MAX.....%llu (0x%llx)            \n", ULLONG_MAX, ULLONG_MAX );
-    // printk( "----------------------------------------\n" );
-    // printk( "String.........%s                       \n", "Hello, World!" );
-    // printk( "Char...........%c                       \n", 'B' );
-    // printk( "Hex............0x%X                     \n", 0xdeadbeef );
-    // printk( "Pointer........%p                       \n", &kernel_main );
-    // printk( "----------------------------------------\n" );
-    // printk( "Done!                                   \n" );
+    printk( "vga_driver_init() succeeded!\n" );
 
     // Initialize the keyboard driver
     if ( keyboard_driver_polling_init() == FAILURE )
@@ -62,15 +38,19 @@ int kernel_main( void )
 
     printk( "keyboard_driver_polling_init() succeeded!\n" );
 
-    // Poll the keyboard driver and print the character to the screen
-    while ( 1 )
-    {
-        char c = keyboard_driver_polling_get_char();
-        if ( c != 0 )
-        {
-            printk( "%c", c );
-        }
-    }
+    //// Poll the keyboard driver and print the character to the screen
+    // while ( 1 )
+    //{
+    //     char c = keyboard_driver_polling_get_char();
+    //     if ( c != 0 )
+    //     {
+    //         printk( "%c", c );
+    //     }
+    // }
+
+    // Run tests
+
+    test_printk();
 
     return 0;
 }
