e3672fc6c2a3781563289223729ce6afb00841ad
Bryce Melander <brycemelander@gmail.com>
2024-01-22 23:50:25 +0000

    Major refactor to only print string after it has been parsed entirely

--- Binux/lib/printk.c
+++ Binux/lib/printk.c
@@ -20,6 +20,10 @@
 #define llong  long long
 #define ullong unsigned llong
 
+#define BASE_8  ( 8U )
+#define BASE_10 ( 10U )
+#define BASE_16 ( 16U )
+
 typedef enum {
     LOWERCASE = 0U,
     UPPERCASE = 32U  // 'a' - 'A' = 32
@@ -27,96 +31,112 @@ typedef enum {
 
 /* Private Functions */
 
-#define print_specifier( sp ) \
-    do                        \
-    {                         \
-        print_char( '%' );    \
-        print_char( sp );     \
-    } while ( 0 )
+#define print_str( s ) vga_display_str( s )
 
-#define print_char( c ) vga_display_char( c )
-#define print_str( s )  vga_display_str( s )
-
-void print_llu( ullong n )
+void format_char( char *str, char c )
 {
-    if ( n / 10 )
-    {
-        print_llu( n / 10 );
-    }
-
-    vga_display_char( (char)( ( n % 10 ) + '0' ) );
+    str[0] = c;
+    str[1] = '\0';
 }
 
-void print_ll( llong n )
+void format_str( char *str, char *s )
 {
-    if ( n < 0 )
+    size_t len = strlen( str );
+
+    if ( len == MAX_STR_LEN - 1 )
     {
-        vga_display_char( '-' );
-        n = -n;
+        OS_ERROR( "String buffer overflow!\n" );
+        return;
     }
 
-    print_llu( (ullong)n );
+    strncat( str, s, strlen( s ) );
 }
 
-void print_hex( uint64_t n, char_case_t char_case )
+void format_llu_base_n( char *str, ullong n, uint8_t base, uint8_t text_case )
 {
-    if ( n / 16 )
+    size_t len = strlen( str );
+
+    if ( len == MAX_STR_LEN - 1 )
     {
-        print_hex( n / 16, char_case );
+        OS_ERROR( "String buffer overflow!\n" );
+        return;
     }
 
-    uint8_t digit = (uint8_t)( n % 16 );
+    uint8_t digit_d = (uint8_t)( n % base );
+    char digit_c;
 
-    if ( digit < 10 )
+    if ( digit_d < 10 )
     {
-        vga_display_char( (char)( digit + '0' ) );
+        digit_c = (char)( digit_d + '0' );
     }
     else
     {
-        vga_display_char( (char)( ( digit - 10 ) + ( 'a' - (int)char_case ) ) );
+        digit_c = (char)( digit_d - 10 + 'A' + text_case );
     }
-}
 
-void print_oct( uint64_t n )
-{
-    if ( n / 8 )
+    str[len] = digit_c;
+    str[len + 1] = '\0';
+
+    if ( n / base )
     {
-        print_oct( n / 8 );
+        format_llu_base_n( str, n / base, base, text_case );
     }
-
-    vga_display_char( (char)( ( n % 8 ) + '0' ) );
 }
 
-#define parse_specifier( sp, args, type )                                                     \
-    do                                                                                        \
-    {                                                                                         \
-        switch ( sp )                                                                         \
-        {                                                                                     \
-            case 'd':                                                                         \
-            case 'i':                                                                         \
-                print_ll( (llong)va_arg( args, type ) );                                      \
-                break;                                                                        \
-                                                                                              \
-            case 'u':                                                                         \
-                print_llu( (ullong)va_arg( args, unsigned type ) ); /* NOLINT */              \
-                break;                                                                        \
-                                                                                              \
-            case 'x':                                                                         \
-                print_hex( (uint64_t)va_arg( args, unsigned type ), LOWERCASE ); /* NOLINT */ \
-                break;                                                                        \
-                                                                                              \
-            case 'X':                                                                         \
-                print_hex( (uint64_t)va_arg( args, unsigned type ), UPPERCASE ); /* NOLINT */ \
-                break;                                                                        \
-                                                                                              \
-            case 'o':                                                                         \
-                print_oct( (uint64_t)va_arg( args, unsigned type ) ); /* NOLINT */            \
-                break;                                                                        \
-                                                                                              \
-            default:                                                                          \
-                print_specifier( sp );                                                        \
-                break;                                                                        \
-        }                                                                                     \
+void format_ll_base_n( char *str, llong n, uint8_t base, uint8_t text_case )
+{
+    if ( n < 0 )
+    {
+        str[0] = '-';
+        format_llu_base_n( str + 1, (ullong)( -n ), base, text_case );
+    }
+    else
+    {
+        format_llu_base_n( str, (ullong)n, base, text_case );
+    }
+}
+
+#define format_ll( str, n )  format_ll_base_n( str, n, BASE_10, LOWERCASE )
+#define format_llu( str, n ) format_llu_base_n( str, n, BASE_10, LOWERCASE )
+
+#define format_hex( str, n, text_case ) format_llu_base_n( str, n, BASE_16, text_case )
+#define format_oct( str, n )            format_llu_base_n( str, n, BASE_8, LOWERCASE )
+
+#define parse_specifier( str, sp, args, type )                                           \
+    do                                                                                   \
+    {                                                                                    \
+        switch ( sp )                                                                    \
+        {                                                                                \
+            case 'd':                                                                    \
+            case 'i':                                                                    \
+                format_ll( str, (llong)va_arg( args, type ) );                           \
+                break;                                                                   \
+                                                                                         \
+            case 'u':                                                                    \
+                format_llu( str, (ullong)va_arg( args, unsigned type ) ); /* NOLINT */   \
+                break;                                                                   \
+                                                                                         \
+            case 'x':                                                                    \
+                format_hex(                                                              \
+                    str, (uint64_t)va_arg( args, unsigned type ), LOWERCASE /* NOLINT */ \
+                );                                                                       \
+                break;                                                                   \
+                                                                                         \
+            case 'X':                                                                    \
+                format_hex(                                                              \
+                    str, (uint64_t)va_arg( args, unsigned type ), UPPERCASE /* NOLINT */ \
+                );                                                                       \
+                break;                                                                   \
+                                                                                         \
+            case 'o':                                                                    \
+                format_oct( str, (uint64_t)va_arg( args, unsigned type ) ); /* NOLINT */ \
+                break;                                                                   \
+                                                                                         \
+            default:                                                                     \
+                format_char( str, '%' );                                                 \
+                format_char( str, sp );                                                  \
+                break;                                                                   \
+        }                                                                                \
     } while ( 0 )
 
 /* Public Functions */
@@ -126,70 +146,82 @@ __attribute__( ( format( printf, 1, 2 ) ) ) int printk( const char *fmt, ... )
     va_list args;
     va_start( args, fmt );
 
-    size_t i, len = strlen( fmt );
+    size_t str_idx, len = strlen( fmt );
 
-    for ( i = 0; i < len; i++ )
+    char str[MAX_STR_LEN] = { 0 };
+
+    for ( str_idx = 0; str_idx < len; str_idx++ )
     {
-        switch ( fmt[i] )
+        switch ( fmt[str_idx] )
         {
             case '%':
-                switch ( fmt[i + 1] )
+                switch ( fmt[str_idx + 1] )
                 {
                     case '%':
-                        print_char( '%' );
+                        format_char( str, '%' );
                         break;
 
                     case 'c':  // Character
-                        print_char( (char)va_arg( args, uint ) );
+                        format_char( str, (char)va_arg( args, uint ) );
                         break;
 
                     case 's':  // String
-                        print_str( va_arg( args, char * ) );
+                        format_str( str, va_arg( args, char * ) );
                         break;
 
                     case 'p':  // Pointer
-                        print_str( "0x" );
-                        print_hex( (uint64_t)va_arg( args, void * ), LOWERCASE );
+                        format_str( str, "0x" );
+                        format_hex( str, (uint64_t)va_arg( args, void * ), LOWERCASE );
                         break;
 
                     case 'l':  // Long
-                        switch ( fmt[i + 2] )
+                        switch ( fmt[str_idx + 2] )
                         {
                             case 'l':
-                                parse_specifier( fmt[i + 3], args, llong );
-                                i++;
+                                parse_specifier( str, fmt[str_idx + 3], args, llong );
+                                str_idx++;
                                 break;
 
                             default:
-                                parse_specifier( fmt[i + 2], args, long );
+                                parse_specifier( str, fmt[str_idx + 2], args, long );
                                 break;
                         }
-                        i++;
+                        str_idx++;
                         break;
 
                     case 'q':  // Long Long NOLINT
-                        parse_specifier( fmt[i + 2], args, llong );
-                        i++;
+                        parse_specifier( str, fmt[str_idx + 2], args, llong );
+                        str_idx++;
                         break;
 
                     case 'h':  // Short
-                        parse_specifier( fmt[i + 2], args, int );
-                        i++;
+                        parse_specifier( str, fmt[str_idx + 2], args, int );
+                        str_idx++;
                         break;
 
                     default:
-                        parse_specifier( fmt[i + 1], args, int );
+                        parse_specifier( str, fmt[str_idx + 1], args, int );
                         break;
                 }
 
-                i++;
+                str_idx++;
                 break;
 
             default:
-                print_char( fmt[i] );
+                format_char( str, fmt[str_idx] );
                 break;
         }
+
+        if ( strlen( str ) == MAX_STR_LEN - 1 )
+        {
+            OS_ERROR( "String buffer overflow!\n" );
+
+            va_end( args );
+            return 1;
+        }
     }
+
+    print_str( str );
 
     va_end( args );
 
