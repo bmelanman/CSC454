a0ac52e2683b58d031f6ab3148f5719423943ba4
Bryce Melander <brycemelander@gmail.com>
2024-02-07 15:37:05 -0800

    starting MMU implementation

---
+++ Bric_OS/src/drivers/mmu_driver.c
@@ -0,0 +1,46 @@
+/** @file mmu_driver.c
+ *
+ * @brief
+ *
+ * @author Bryce Melander
+ * @date Feb-07-2024
+ *
+ * @copyright (c) 2024 by Bryce Melander under MIT License. All rights reserved.
+ * (See http://opensource.org/licenses/MIT for more details.)
+ */
+
+#include "mmu_driver.h"
+
+/* Private Includes */
+
+#include "common.h"
+
+/* Private Defines and Macros */
+
+/* Private Types and Enums */
+
+/* Global Variables */
+
+/* Private Functions */
+
+/* Public Functions */
+
+void MMU_init( void *tag_ptr )
+{
+    //
+    (void)tag_ptr;
+}
+
+void *MMU_pf_alloc( void )
+{
+    // comment
+    return NULL;
+}
+
+void MMU_pf_free( void *pf )
+{
+    //
+    (void)pf;
+}
+
+/*** End of File ***/
---
+++ Bric_OS/src/drivers/mmu_driver.h
@@ -0,0 +1,31 @@
+/** @file mmu_driver.h
+ *
+ * @brief
+ *
+ * @author Bryce Melander
+ * @date Feb-07-2024
+ *
+ * @copyright (c) 2024 by Bryce Melander under MIT License. All rights reserved.
+ * (See http://opensource.org/licenses/MIT for more details.)
+ */
+
+#ifndef MMU_DRIVER_H
+# define MMU_DRIVER_H
+
+/* Includes */
+
+/* Defines */
+
+/* Macros */
+
+/* Typedefs */
+
+/* Public Functions */
+
+void *MMU_pf_alloc( void );
+
+void MMU_pf_free( void *pf );
+
+#endif /* MMU_DRIVER_H */
+
+/*** End of File ***/
